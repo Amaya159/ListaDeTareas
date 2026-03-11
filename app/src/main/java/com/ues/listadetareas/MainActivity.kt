b262fe84bb9a14d5dfdcd6d556797bd9ec8c7795
Jonathan Amayay <jonafigueroa@gmail.com>
2026-03-11 13:39:03 -0700

    Actividad 1: Crear el proyecto, organizar estructura limpia y conexión con GitHub

---
+++ app/src/main/java/com/ues/listadetareas/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ues.listadetareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ues.listadetareas.ui.theme.ListaDeTareasTheme
+import com.ues.listadetareas.ui.theme.screens.TaskListScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            TaskListScreen()
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListaDeTareasTheme {
+        Greeting("Android")
+    }
+}
